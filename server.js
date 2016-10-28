c5c6139867451fb1d835aeade72b7a8645b41be4
ShadyXV <shovethatshitin@gmail.com>
2016-10-29 01:29:11 +0545

    Prep repo for heroku!

--- server.js
+++ server.js
@@ -4,16 +4,37 @@ var express = require('express');
 // create our app
 //var app express library called as a function and no args.
 var app = express();
-
+//heroku
+const PORT =process.env.PORT || 3000;
 //tell it which folder u want to serve
 //app.use add functionality to ur express apps
 //express.static is gonna specify a folder name u want to expose to the server
 //in this case public is the folder
+
+//common pattern for express middleware
+app.use(function(req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'htto'){
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
+
 app.use(express.static('public'));
 //now start the server
-//app.listen takes 2 args first port and then the fn to call once the server is up.
-app.listen(3000,function(){
-  console.log('express server is up on port 3000')
+//app.listen takes 2 args first PORT and then the fn to call once the server is up.
+app.listen(PORT,function(){
+  console.log('express server is up on PORT ' + port);
 });
 //in the root of the app create the public folder--helloReact
-//index.html is the default for the app
+//inde
+
+//common pattern for express middleware
+app.use(function(req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'htto'){
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
+x.html is the default for the app
